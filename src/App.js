dbf6e82082ee3cbaa5f6fd6ff6413c131fb0540d
omamaintikhab <omamaintikhab97@gmail.com>
2020-07-24 21:51:46 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
+import Products from './components/products';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size:"",
+      sort:""
+
+    }
+  }
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">Shopping Cart</a>
       </header>
-      <main>Product List</main>
+      <main>Product List
+        <div className="content">
+          <div className="main">
+            <Products
+            products={this.state.products}
+            ></Products>
+          </div>
+          <div className="sidebar">
+            cartitems
+          </div>
+        </div>
+
+      </main>
       <footer>All right reserved</footer>
     </div>
   );
 }
-
+}
 export default App;
